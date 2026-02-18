a7637fdaecdc862753ecfc46240356f073e3024e
akrs-code <abdulkhaliqsolaiman@gmail.com>
2026-02-18 23:23:04 +0800

    Add index view to list all tours

---
+++ worldtour/asiatoursagency/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Tour
+
+def index(request):
+    tours = Tour.objects.all()
+    context = {"tours": tours}
+    return render(request, "tours/index.html", context)
